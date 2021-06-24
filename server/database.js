7fa4bf514c34ffb887b3dcee3ecd53440c03b328
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-06-25 02:20:27 +0600

    Add mysql connection port

--- server/database.js
+++ server/database.js
@@ -8,13 +8,13 @@ const connection = mysql.createConnection({
     host     : process.env.MYSQL_HOST,
     user     : process.env.MYSQL_USER,
     password : process.env.MYSQL_PASSWORD,
-    database : process.env.MYSQL_TABLE
+    database : process.env.MYSQL_TABLE,
+    port     : process.env.MYSQL_PORT
 })
 
 connection.connect(function(err) {
     if (err) {
-        console.error('error connecting: ' + err.sqlMessage);
-        return;
+        throw err;
     }
 
     console.log('connected as id ' + connection.threadId);
