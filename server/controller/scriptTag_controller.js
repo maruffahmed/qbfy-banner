f85bc2b06ef7b403274279a3ffbec58733326a25
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-06-26 16:27:52 +0600

    Script tag controller code refector

--- server/controller/scriptTag_controller.js
+++ server/controller/scriptTag_controller.js
@@ -1,57 +1,74 @@
 import { DataType } from "@shopify/shopify-api";
 
 // Controller for create an script tag
-export async function createScriptTag(client) {
-  // Check shopify client
-  if (!client) {
-    // If client didn't found
+export async function createScriptTag(ctx) {
+  const { myClient } = ctx;
+  // Check the shopify client
+  if (!myClient) {
     throw new Error("Client didn't found");
   }
 
   try {
+    // Try to create script tag
     const data = {
       script_tag: {
         event: "onload",
         src: "https://facebook.com",
       },
     };
-    const result = await client.post({
+    const result = await myClient.post({
       path: "script_tags",
       data,
       type: DataType.JSON,
     });
-    return result.body;
+    // script tag create success response
+    ctx.body = result.body;
   } catch (error) {
-    return error;
-  }
-}
-// Contoller for geting script tags
-export async function getScriptTags(client, query) {
-  if (!client) {
-    throw new Error("Client didn't found");
-  }
-  try {
-    const result = await client.get({
-      path: "script_tags",
-      query,
-    });
-    return result.body;
-  } catch (error) {
-    return error;
+    // string tag create fail response
+    ctx.body = {error : error.message};
   }
 }
 
-export async function deleteScriptTags(client, scriptId) {
-  if (!client) {
+// Contoller for geting script tags
+export async function getScriptTags(ctx) {
+  // Script tag Search query
+  const query = ctx.query;
+  const { myClient } = ctx;
+  // Check the shopify client
+  if (!myClient) {
+    throw new Error("Client didn't found");
+  }
+
+  try {
+    // Try to get all the script tags
+    const result = await myClient.get({
+      path: "script_tags",
+      query,
+    });
+    // Get Script tag success response
+    ctx.body = result.body;
+  } catch (error) {
+    // Get script tag fail response
+    ctx.body = {error : error.message};
+  }
+}
+
+export async function deleteScriptTags(ctx) {
+  // Script tag id
+  const { scriptId } = ctx.params;
+  const { myClient } = ctx;
+  // Check the shopify client
+  if (!myClient) {
     throw new Error("Client didn't found");
   }
   try {
-    const result = await client.delete({
+    // Try to delete script tag
+    const result = await myClient.delete({
       path: `script_tags/${scriptId}`,
     });
-    return result.body;
+    ctx.body = { message: "Script tag delete successfully" };
   } catch (error) {
-    return error;
+    ctx.body = { error: result.message };
   }
 }
 
--- server/router/script_tag.js
+++ server/router/script_tag.js
@@ -3,43 +3,14 @@ import {
   createScriptTag,
   getScriptTags,
   deleteScriptTags,
-} from "../controller/script_tag_controller";
+} from "../controller/scriptTag_controller";
 const router = new Router({ prefix: "/script_tags" });
 
 // Route for get all script tags
-router.get("/", async (ctx) => {
-  const query = ctx.query;
-  const { myClient } = ctx;
-  const result = await getScriptTags(myClient, query);
-  if (result.script_tags) {
-    ctx.body = result;
-  } else {
-    ctx.body = { error: result.message };
-  }
-});
-
-router.post("/", async (ctx) => {
-  const { myClient } = ctx;
-  // Call the controller
-  const result = await createScriptTag(myClient);
-  // Give response
-  if (result.script_tag) {
-    ctx.body = result;
-  } else {
-    ctx.body = { error: result.message };
-  }
-});
-
+router.get("/", getScriptTags);
+// Route for create script tags
+router.post("/", createScriptTag);
 // Route for delete an script tag by id
-router.delete("/:scriptId", async (ctx) => {
-  const { scriptId } = ctx.params;
-  const { myClient } = ctx;
-  const result = await deleteScriptTags(myClient, scriptId);
-  if (typeof result === "string") {
-    ctx.body = { error: result.message };
-  } else {
-    ctx.body = { message: "Script tag delete successfully" };
-  }
-});
+router.delete("/:scriptId", deleteScriptTags);
 
 export default router;
